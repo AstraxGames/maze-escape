a400e73d5d74f29d4e1d9f0fd8db5931d80349b7
AstraxGames <ranjanroushan5@gmail.com>
2026-07-28 23:52:13 +0530

    Added auto-updater and publish configs

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 const { app, BrowserWindow, globalShortcut } = require('electron');
+const { autoUpdater } = require('electron-updater');
+
+let win;
 
 function createWindow() {
-    const win = new BrowserWindow({
+    win = new BrowserWindow({
         fullscreen: true,
-        autoHideMenuBar: true, // Hides the default menu (Alt key shows it)
+        autoHideMenuBar: true,
+        icon: __dirname + '/icon.ico',
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: false
@@ -12,18 +16,40 @@ function createWindow() {
 
     win.loadFile('index.html');
 
-    // Intercept F11 to prevent window resizing
+    // Intercept F11 to cleanly toggle fullscreen without small window bugs
     globalShortcut.register('F11', () => {
-        const isFullScreen = win.isFullScreen();
-        win.setFullScreen(!isFullScreen);
+        if (win) {
+            const isFullScreen = win.isFullScreen();
+            win.setFullScreen(!isFullScreen);
+        }
     });
+
+    // Check for updates automatically in background after launching
+    autoUpdater.checkForUpdatesAndNotify();
 }
+
+// Auto-updater logs & events
+autoUpdater.on('update-available', () => {
+    console.log('Update available. Downloading...');
+});
+
+autoUpdater.on('update-downloaded', () => {
+    // Automatically restart to install update
+    autoUpdater.quitAndInstall();
+});
 
 app.whenReady().then(() => {
     createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
 });
 
-// Cleanup on exit
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
+
 app.on('will-quit', () => {
     globalShortcut.unregisterAll();
 });
